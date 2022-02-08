c083c3598e434c1f864efdc25bb0ab8d1fcc1fe3
ezequiel <ezequiel.ledesma026@gmail.com>
2022-02-07 22:50:25 -0300

    Se agregaron nuevos ejemplos

---
+++ 02-Fundamentos/template-string.js
@@ -0,0 +1,18 @@
+
+const nombre = 'Deadpool';
+const real = 'Wade Winston';
+
+const normal = nombre +' '+ real;
+//otra forma
+const template = `${nombre} ${real}`;
+
+console.log(normal);
+console.log(template);   
+
+const html = `
+<h1> Hola </h1>
+<p>Mundo</p>`;
+
+console.log(html);
+
+//console.log(normal == template);
