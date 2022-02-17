f0d279e266865a5a7bbab69c8038363455ff3dfd
ezequiel <ezequiel.ledesma026@gmail.com>
2022-02-16 22:49:22 -0300

    se suben ejemplos

---
+++ 03-bases-node/app.js
@@ -0,0 +1,14 @@
+
+//const multiplicar = () => {
+    
+console.clear();
+console.log('===================');
+console.log('Tabla del 5');
+console.log('===================');
+
+    const  base = 5 ;
+  
+    for ( let i = 1 ; i <= 10 ; i++  ){
+    console.log(`${base} x ${i} = ${base *  i}`);
+    }
+//}
---
+++ 03-bases-node/grabarLaTabla.js
@@ -0,0 +1,28 @@
+//importo la libreria de file system
+
+const fs = require ('fs');
+
+//const multiplicar = () => {
+    
+console.clear();
+console.log('===================');
+console.log('Tabla del 5');
+console.log('===================');
+
+    const  base = 5 ;
+    let salida = '';
+  
+    for ( let i = 1 ; i <= 10 ; i++  ){
+   // console.log(`${base} x ${i} = ${base *  i}`);
+   //el dato de la salida
+    salida += `${base} x ${i} = ${base *  i}\n`;
+    console.log(salida);
+    }
+
+    //el callback lo saco de la documentacion del servicio para cuando hay error
+    //archivo a crear, salida , error
+    fs.writeFile(`tabla-${base}.txt`, salida,(err)=> {
+        if (err) throw err;
+        console.log(`tabla-${base} creada con exito`);
+    });
+//}
