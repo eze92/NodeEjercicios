3f6d990134150d7b369c9187d72fe46585541ba4
ezequiel <ezequiel.ledesma026@gmail.com>
2022-02-20 13:45:11 -0300

    se modifica a fines educativo para ingrear el valor a multiplicar por consola

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -5,10 +5,21 @@ const{crearArchivo} = require('./helpers/multiplicar');
  
 console.clear();
 
+//me dice donde se crea node de modo global y donde se encuentra la app ejecutada
+//console.log(process.argv);
 
-const  base = 2 ;
+const [,,arg3 = 'base=5'] = process.argv;
+//el split me divide un string en un  arreglo por lo que arg3 pasa a ser argumento uno y base el 2, en este caso serapa a partir del igual
+const [,base=5] = arg3.split('=');
+
+
+//console.log(base);
+
+//const  base = 2 ;
   
 
 crearArchivo(base)
     .then(nombreArchivo => console.log(nombreArchivo,'creado'))
     .catch(error => console.log(error));
+
+    
