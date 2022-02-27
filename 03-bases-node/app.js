3859d3e899cbe12d2a5433a0d1e7b59233646036
ezequiel <ezequiel.ledesma026@gmail.com>
2022-02-27 13:40:29 -0300

    se modulariza el yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -2,32 +2,14 @@
 //exporto la funcion crear archivo desde multiplocar 
 //const { argv } = require('process');
 
-const{crearArchivo} = require('./helpers/multiplicar');
-//como importar esta en la documentacion
-const argv = require('yargs')
-                .option('b',{
-                    alias: 'base',
-                    type: 'number',
-                    demandOption: true
-                })
-                .option('l',{
-                    alias: 'listar',
-                    type: 'boolean',
-                    demandOption: true,
-                    default: false
-                })
-                .check((argv, options) => {
-                    if(isNaN (argv.b)) {
-                        throw 'La base tiene que ser un numero'
-                    }
-                    return true;
-                })      
-                .argv;
+const {crearArchivo} = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
 
                  
 console.clear();
 
-console.log(argv);
+//para ver como funciona yars
+//console.log(argv);
 
 console.log('base:yargs',argv.b);
   
@@ -36,4 +18,3 @@ crearArchivo(argv.b, argv.l)
     .then(nombreArchivo => console.log(nombreArchivo,'creado'))
     .catch(error => console.log(error));
 
-    
---
+++ 03-bases-node/config/yargs.js
@@ -0,0 +1,25 @@
+//como importar esta en la documentacion
+const argv = require('yargs')
+//opciones que va tener para ingresar por consola
+        .option('b',{
+             alias: 'base',
+             type: 'number',
+             demandOption: true,
+             describe : 'Es la base de la tabla de multiplicar'
+            })
+        .option('l',{
+             alias: 'listar',
+             type: 'boolean',
+          //   demandOption: true,
+             default: false,
+             describe : 'Lista la tabla de multiplicar en consola'
+            })
+        .check((argv, options) => {
+             if(isNaN (argv.b)) {
+                throw 'La base tiene que ser un numero'
+             }
+            return true;
+            })      
+        .argv;
+
+module.exports = argv;
