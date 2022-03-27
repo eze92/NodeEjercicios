b6d103204b1467332a9e79614991350cbd72fd17
ezequiel <ezequiel.ledesma026@gmail.com>
2022-03-27 11:43:12 -0300

    se modifica el index

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,29 +1,53 @@
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer")
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/Busquedas");
 
 const main = async() => {
+
+    //creo instancia de la clase 
+    const busquedas = new Busquedas();
 
     let opt = 0;
 
     do{
         opt = await inquirerMenu();
-        console.log({opt})
+        
+        switch (opt) {
+            case 1:
+                //Mostrar mensaje 
+                const lugar =  await leerInput('Ciudad:');
+                console.log(lugar);
+
+                //Buscar los lugares 
+
+                /// Seleccionar el lugar 
+
+                //Clima 
+
+                //Mostrar resultados
+
+                console.log('\n Informacion de la ciudad \n'.green);
+                console.log('Ciudad:',);
+                console.log('Latitud:',);
+                console.log('Longitud:',);
+                console.log('Mínima:',);
+                console.log('Máxima:',);
+
+
+            break;
+
+            case 2:
+                
+            break;
+
+            case 0:
+                
+            break;
+        }
 
         if (opt !==0)
         await pausa();
 
-        /*switch (opt) {
-            case 1:
-                
-                break;
-
-            case 2:
-                
-                break;
-
-            case 0:
-                
-                 break;
-        }*/
+        
     }
     while ( opt != 0);
 }
