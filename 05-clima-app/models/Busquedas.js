8137243256ca138f1e7fcc1c7e4ef04b84ab4dff
ezequiel <ezequiel.ledesma026@gmail.com>
2022-04-03 12:10:57 -0300

    Se configura el acceso http

--- 05-clima-app/models/Busquedas.js
+++ 05-clima-app/models/Busquedas.js
@@ -4,8 +4,17 @@ class Busquedas {
 
     historial = ['Buenos Aires','La Plata','San Luis'];
 
-    constructor(){
+    constructor() {
         //Leer DB si existe
+    }
+
+    //returno un objeto
+    get paramsMapBox() {
+        return {
+            'access_token': 'pk.eyJ1IjoicGVwZTE5OTIiLCJhIjoiY2wxamR3Y3ZqMXFydjNqcDhiMmVxbWk3YSJ9.ksvvk12OOoodL7GUb9WtAw',
+            'limit':5,
+            'lenguaje': 'es'
+        }
     }
 
     //metodo asincrono porque va a realizar una peticion http
@@ -13,12 +22,22 @@ class Busquedas {
         
         try{
         //peticion http
+        const instance = axios.create({
+           
+
+            baseURL:` https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+            //son todos aquellos que vienen despues del signo de pregunta
+            params: this.paramsMapBox
+            
+        });
+
+        const resp = await instance.get();
 
         //console.log(lugar);
-            const resp = await axios.get('https://reqres.in/api/users?page=2');
-            console.log(resp.data);
+        // const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/buenos%20aires.json?limit=5&proximity=ip&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1IjoicGVwZTE5OTIiLCJhIjoiY2wxamR3Y3ZqMXFydjNqcDhiMmVxbWk3YSJ9.ksvvk12OOoodL7GUb9WtAw');
+         console.log(resp.data);
 
-             return []; //retorar los lugares
+            return []; //retorar los lugares
         }
         catch (error){
             return []; 
