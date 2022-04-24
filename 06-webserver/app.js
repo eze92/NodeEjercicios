9a222eb6656301fbd3ff7228d829f04f03ebad9f
ezequiel <ezequiel.ledesma026@gmail.com>
2022-04-24 11:52:54 -0300

    se agrega contenido estatico

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,16 +2,23 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+//Servir contenido estatico
+
+app.use( express.static('public'));
+
+/* esta parte no se ejecuta al estar el contenido estatico
 app.get('/', (req, res) => {
   res.send('Home Page')
-})
+})*/
 
 app.get('/hola-mundo', (req, res) => {
     res.send('Hola mundo en su respectiva ruta')
   })
 
 app.get('*', (req, res) =>{
-  res.send('404 | Page not found')
+  //res.send('404 | Page not found')
+  //debo construir la ruta 
+  res.sendFile(__dirname + '/public/404.html');
 })
 
 app.listen(port, () => {
