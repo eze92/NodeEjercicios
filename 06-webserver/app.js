4f1efb322ec794ea0c822e8bb4c97ef130528e55
ezequiel <ezequiel.ledesma026@gmail.com>
2022-05-01 12:35:06 -0300

    add variable entorno para puerto 8081

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express')
 const hbs = require('hbs');
 
 const app = express()
-const port = 8080;
+const port = process.env.PORT;
 
 //Handlebars
 app.set('view engine', 'hbs');
